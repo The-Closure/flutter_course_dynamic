de4c8f872272d9a269459d40338bec0a67506cbe
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-10-05 07:48:50 -0700

    create dart porject

---
+++ bin/git_session.dart
@@ -0,0 +1,5 @@
+import 'package:git_session/git_session.dart' as git_session;
+
+void main(List<String> arguments) {
+  print('Hello world: ${git_session.calculate()}!');
+}
---
+++ test/git_session_test.dart
@@ -0,0 +1,8 @@
+import 'package:git_session/git_session.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

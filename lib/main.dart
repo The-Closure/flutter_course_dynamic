818ab4a7cbd249a90427389271adeba5078a3a9f
AbdalazizTll <abdalaziztll992010@gmail.com>
2023-11-06 08:05:27 -0800

    statfull and git

--- lib/main.dart
+++ lib/main.dart
@@ -9,145 +9,120 @@ main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext contes) {
-    return MaterialApp(
-      home: CustomizePage(),
-    );
+    return MaterialApp(home: SplashScreen());
   }
 }
 
-class BasePage extends StatelessWidget {
-  const BasePage({
-    super.key,
-  });
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+@override
+  void initState() {
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(),));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomePage(),
-                    ));
-              },
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-                child: Hero(
-                  tag: 'anime',
-                  child: Center(child: Text('Hello world')),
-                ),
-              ),
-            ),
-            InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Text('prevouis page')),
-            Hero(
-                tag: 'logo',
-                child: FlutterLogo(
-                  size: 200,
-                ))
-          ],
-        ),
-      ),
+      body: Center(child: Icon(Icons.facebook,color: Colors.blue),),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  bool logic = true;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: ListTile(
-        leading: Hero(
-          tag: 'logo',
-          child: FlutterLogo(),
-        ),
-        trailing: Checkbox(
-          onChanged: (value) {
-            setState(() {
-              logic = value!;
-            });
-            print(logic);
-          },
-          value: logic,
-        ),
-      ),
-    );
-  }
-}
-
-class CustomizePage extends StatefulWidget {
-  const CustomizePage({super.key});
-
-  @override
-  State<CustomizePage> createState() => _CustomizePageState();
-}
-
-class _CustomizePageState extends State<CustomizePage> {
   double width = 200;
   double height = 200;
   Color color = Colors.red;
   bool magic = true;
+
+
+
+    @override
+    initState() {
+      Future.delayed(Duration(seconds: 4), () {
+        print('-------------------');
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MyWidget(),
+            ));
+      });
+    }
+
   @override
   Widget build(BuildContext context) {
+  
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            InkWell(
-              onTap: () {
-                print('object');
-                setState(() {
-                  width = width + 30;
-                  height = height + 30;
-                  color = Colors.blue;
-                });
-              },
-              child: Visibility(
-                replacement: FlutterLogo(
-                  size: 300,
-                  duration: Duration(seconds: 4),
-                ),
-                visible: magic,
-                child: AnimatedContainer(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: color,
-                  ),
-                  duration: Duration(seconds: 1),
+            Visibility(
+              replacement: FlutterLogo(
+                size: 200,
+              ),
+              visible: magic,
+              child: AnimatedContainer(
+                  duration: Duration(seconds: 2),
                   width: width,
                   height: height,
-                ),
-              ),
+                  color: color),
             ),
-            InkWell(
-                onTap: () {
-                  setState(() {
-                    magic = !magic;
-                  });
-                },
-                child: Text(magic ? 'hide' : 'show'))
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (magic) {
+                      setState(() {
+                        print('object');
+                        width = width + 100;
+                        height = height + 100;
+                        color = Colors.blue;
+                        magic = !magic;
+                      });
+                    } else {
+                      setState(() {
+                        width = width - 100;
+                        height = height - 100;
+                        color = Colors.red;
+                        magic = !magic;
+                      });
+                    }
+                  },
+                  child: Text(magic ? 'zoom in' : 'zoom out')),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+
+
